90b0dfd0a91836d05a897a23f25ed3604d239af8
unknown <sachanadharul@ymail.com>
2023-02-10 16:14:47 +0700

    chore: add and setup 'Oh-Vue-Icons' dependencies

--- src/main.js
+++ src/main.js
@@ -6,7 +6,12 @@ import store from './store'
 import './style.scss'
 import App from './App.vue'
 
+import { OhVueIcon, addIcons } from 'oh-vue-icons'
+import { HiShoppingCart } from 'oh-vue-icons/icons'
+addIcons(HiShoppingCart)
+
 createApp(App)
+.component('vIcon', OhVueIcon)
 .use(router)
 .use(store)
 .mount('#app')
