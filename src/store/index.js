f897f1afde7988d50d43b7d5a47bb20781385a5d
unknown <sachanadharul@ymail.com>
2023-02-11 16:37:07 +0700

    feat: setup sanity configuration to get data from sanity cms

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,28 @@
 import { createStore } from "vuex"
 
+import { urlFor, sanityApi } from '../_api/sanity-client'
+
 const store = createStore({
   state: {
-
+    menus: {
+      data: {}
+    }
   },
-  action: {
-
+  actions: {
+    async getMenus({ commit }) {
+      const query = '*[_type == "menus"]'
+      const allMenus = await sanityApi.fetch(query)
+        try {
+          commit("GET_MENUS", allMenus)
+        } catch (err) {
+          throw err
+        }
+    }
   },
   mutations: {
-
+    GET_MENUS: (state, menus) => {
+      state.menus.data = menus
+    }
   }
 })
 
