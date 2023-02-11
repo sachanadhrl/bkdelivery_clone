67a53cff453fcfe4bf587bbd29bea094a3b1b871
unknown <sachanadharul@ymail.com>
2023-02-11 14:48:17 +0700

    feat: create routing for menu & promo and its components

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,25 @@
 import { createRouter, createWebHistory } from "vue-router"
 
 const Index = () => import("../view/Index.vue")
+const Menu = () => import("../view/menu/Menu.vue")
+const Promos = () => import("../view/promo/Promos.vue")
 
 const routes = [
   {
     path: "/",
     name: "index",
     component: Index
-  }
+  },
+  {
+    path: "/menus",
+    name: "menu",
+    component: Menu
+  },
+  {
+    path: "/promotions",
+    name: "promotions",
+    component: Promos
+  },
 ]
 
 const router = createRouter({
---
+++ src/view/promo/Promos.vue
@@ -0,0 +1,3 @@
+<template>
+  Promos
+</template>
