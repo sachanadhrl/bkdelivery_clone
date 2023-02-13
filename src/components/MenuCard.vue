5a177bc13299238b0771b5bc7955a616c985c68a
unknown <sachanadharul@ymail.com>
2023-02-13 15:54:17 +0700

    feat: add carousel and card components on index page

---
+++ src/components/MenuCard.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { useRouter } from 'vue-router'
+
+import { urlFor } from '../_api/sanity-client'
+
+const router = useRouter()
+
+const props = defineProps({
+  allMenus: Object
+})
+</script>
+
+<template>
+  <div v-for="menu in allMenus" :key="menu.menu_slug?.current"
+    @click="router.push({ name: 'menus-variant', params: { slug: menu.menu_slug?.current } })"
+    class="card cursor-pointer">
+    <div class="card-header">
+      <img :src="urlFor(menu.menu_image)" :alt="menu.menu_name" class="card-img">
+    </div>
+    <div class="card-body flex justify-between items-start gap-2">
+      <h2 class="card-title">
+        {{ menu.menu_name }}
+      </h2>
+      <router-link :to="{ name: 'menus-variant', params: { slug: menu.menu_slug?.current } }"
+        class="btn btn-primary hidden lg:block">
+        Order
+      </router-link>
+    </div>
+  </div>
+</template>
--- src/view/Index.vue
+++ src/view/Index.vue
@@ -1,5 +1,25 @@
+<script setup>
+import { computed } from 'vue'
+
+import store from '../store'
+import MenuCard from '../components/MenuCard.vue'
+import Carousel from '../components/Carousel.vue'
+
+const allMenus = computed(() => store.state.menus.data)
+store.dispatch("getMenus")
+
+</script>
+
 <template>
-  <div>
-    Index
+  <!-- <Carousel /> -->
+
+  <div class="container">
+    <h1 class="text-center md:text-3xl font-flame-bold text-yellow-800 my-6">
+      Our Menus
+    </h1>
+
+    <div class="grid grid-cols-2 lg:grid-cols-3 gap-5">
+      <MenuCard :allMenus="allMenus" />
+    </div>
   </div>
 </template>
